5e08a445b097316db4620dfbef3683b389d7cfe1
Aitor Zabarte <aitorzabarte@outlook.es>
2020-09-24 16:34:42 +0200

    Second Commit

--- PruebaClasePrimeraSubida/PruebaClasePrimeraSubida/Program.cs
+++ PruebaClasePrimeraSubida/PruebaClasePrimeraSubida/Program.cs
@@ -7,6 +7,7 @@ namespace PruebaClasePrimeraSubida
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
